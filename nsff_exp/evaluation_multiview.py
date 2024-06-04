42fd52b34ac29844610d461fdd2783c6f22b5576
harshamupparaju <harshadiablo@gmail.com>
2024-06-04 14:24:47 +0530

    Minor changes

--- nsff_exp/evaluation_multiview.py
+++ nsff_exp/evaluation_multiview.py
@@ -322,7 +322,7 @@ def evaluation():
             temp = np.clip(rgb, 0, 1)
             temp = (temp * 255).astype(np.uint8)
             temp = cv2.cvtColor(temp, cv2.COLOR_RGB2BGR)
-            os.mkdir(os.path.join(basedir, expname, 'rgb'), exist_ok=True)
+            os.mkdir(os.path.join(basedir, expname, 'rgb'))
             cv2.imwrite(os.path.join(basedir, expname, 'rgb', '%05d.png'%img_i), temp)
 
             gt_img_path = os.path.join(args.datadir, 
